5567a3f1c73425c24bb4e08a1f6fc2e19ba3b429
Wen Ow <ow.wen2022@gmail.com>
2025-06-10 12:02:51 -0400

    Added View and Delete Routes

--- aircraftHangar/aircraft/urls.py
+++ aircraftHangar/aircraft/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path, include
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # Create Aircraft Route
-    path('create/', views.addView, name='create_aircraft')
+    path('create/', views.addView, name='create_aircraft'),
+    path('view/<int:pk>/', views.viewAircraft, name='view_aircraft'),
+    path('delete/<int:pk>/', views.deleteAircraft, name='delete_aircraft'),
 ]
+
+# Add Static Files URL
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
