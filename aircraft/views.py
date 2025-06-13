7abc022b4a3d6a12a13dd95af57fa3daaa4cb1d0
Wen Ow <ow.wen2022@gmail.com>
2025-06-13 16:58:56 -0400

    Create the detail view page

--- aircraft/views.py
+++ aircraft/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import Aircraft, AircraftType, Category
 from .forms import AddAircraftForm
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='login')
 def addView(request):
     if request.method == 'POST':
         # Handle form submission
@@ -25,6 +27,7 @@ def addView(request):
         }
         return render(request, 'aircraft/add.html', context)
     
+@login_required(login_url='login')
 def viewAircraft(request, pk):
     # Fetch the aircraft entry by primary key (pk)
     aircraft = Aircraft.objects.get(pk=pk)
@@ -33,6 +36,34 @@ def viewAircraft(request, pk):
     }
     return render(request, 'aircraft/view.html', context)
 
+@login_required(login_url='login')
+def editAircraft(request, pk):
+    # Fetch the aircraft entry by primary key (pk)
+    aircraft = Aircraft.objects.get(pk=pk)
+    
+    if request.method == 'POST':
+        # Handle form submission
+        form = AddAircraftForm(request.POST, request.FILES, instance=aircraft)
+        if form.is_valid():
+            form.save()  # Save the updated aircraft entry to the database
+            return redirect('home')  # Redirect to the home page after successful update
+    else:
+        # Pre-fill the form with existing data
+        form = AddAircraftForm(instance=aircraft)
+     # Fetch all aircraft types to display in the form
+        aircraft_types = AircraftType.objects.all()
+        # Fetch all categories to display in the form
+        categories = Category.objects.all()
+           
+        context = {
+            'form': form,
+            'aircraft': aircraft,
+            'aircraft_types': aircraft_types,
+                'categories': categories,
+        }
+        return render(request, 'aircraft/edit.html', context)
+
+@login_required(login_url='login')
 def deleteAircraft(request, pk):
     # Fetch the aircraft entry by primary key (pk)
     aircraft = Aircraft.objects.get(pk=pk)
